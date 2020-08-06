96c9c9aa8aff10491a854c94d2227e9b2cb2016b
Jonathan Hanson <jonathan@jonathan-hanson.org>
2020-08-06 11:08:11 -0700

    roughed in the materials list count, in the footer

--- src/components/3d_editor_selection_info/EditorSelectionInfo.js
+++ src/components/3d_editor_selection_info/EditorSelectionInfo.js
@@ -4,13 +4,21 @@ class EditorSelectionInfo extends React.Component {
 
     render() {
         return (
-            <div className="text-center p-t-20">
-                <p></p>
+            <div className="text-left p-t-10">
+                <span className="p-l-20">
+                    Material Count: {this.props.material_count }
+                </span>
             </div>
         )
     }
 }
 
-EditorSelectionInfo.propTypes = {};
+EditorSelectionInfo.propTypes = {
+    material_count: React.PropTypes.number
+};
+
+EditorSelectionInfo.defaultProps = {
+    material_count: 1
+}
 
 export default EditorSelectionInfo;
